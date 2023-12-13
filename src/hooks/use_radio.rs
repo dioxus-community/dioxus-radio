353dcea7dc9622d3ca48183b6e7764eef43410f3
marc2332 <mespinsanz@gmail.com>
2023-12-13 22:53:53 +0100

    chore: Less lore

--- src/hooks/use_radio.rs
+++ src/hooks/use_radio.rs
@@ -13,79 +13,79 @@ pub trait RadioChannel: PartialEq + Eq + Clone {}
 
 impl<T> RadioChannel for T where T: PartialEq + Eq + Clone {}
 
-pub struct RadioStation<Sound, Channel>
+pub struct RadioStation<Value, Channel>
 where
     Channel: RadioChannel,
 {
-    sound: Rc<RefCell<Sound>>,
+    value: Rc<RefCell<Value>>,
     listeners: Rc<RefCell<HashMap<ScopeId, Channel>>>,
-    play_button: Arc<dyn Fn(ScopeId) + Send + Sync>,
+    schedule_update_any: Arc<dyn Fn(ScopeId) + Send + Sync>,
 }
 
-impl<Sound, Channel> Clone for RadioStation<Sound, Channel>
+impl<Value, Channel> Clone for RadioStation<Value, Channel>
 where
     Channel: RadioChannel,
 {
     fn clone(&self) -> Self {
         Self {
-            sound: self.sound.clone(),
+            value: self.value.clone(),
             listeners: self.listeners.clone(),
-            play_button: self.play_button.clone(),
+            schedule_update_any: self.schedule_update_any.clone(),
         }
     }
 }
 
-impl<Sound, Channel> RadioStation<Sound, Channel>
+impl<Value, Channel> RadioStation<Value, Channel>
 where
     Channel: RadioChannel,
 {
-    pub fn listen(&self, channel: Channel, scope_id: ScopeId) {
+    pub(crate) fn listen(&self, channel: Channel, scope_id: ScopeId) {
         let mut listeners = self.listeners.borrow_mut();
         listeners.insert(scope_id, channel);
     }
 
-    pub fn unlisten(&self, scope_id: ScopeId) {
+    pub(crate) fn unlisten(&self, scope_id: ScopeId) {
         let mut listeners = self.listeners.borrow_mut();
         listeners.remove(&scope_id);
     }
 
-    pub fn play(&self, channel: &Channel) {
+    pub(crate) fn notify_listeners(&self, channel: &Channel) {
         let listeners = self.listeners.borrow();
 
         for (scope_id, listener_channel) in listeners.iter() {
             if listener_channel == channel {
-                (self.play_button)(*scope_id)
+                (self.schedule_update_any)(*scope_id)
             }
         }
     }
 
-    pub fn get_scope_channel(&self, scope_id: ScopeId) -> Channel {
+    pub(crate) fn get_scope_channel(&self, scope_id: ScopeId) -> Channel {
         let listeners = self.listeners.borrow();
         listeners.get(&scope_id).unwrap().clone()
     }
 }
 
-pub struct Radioantenna<Sound, Channel>
+pub struct RadioAntenna<Value, Channel>
 where
     Channel: RadioChannel,
 {
-    station: RadioStation<Sound, Channel>,
+    station: RadioStation<Value, Channel>,
     scope_id: ScopeId,
 }
 
-impl<Sound, Channel> Radioantenna<Sound, Channel>
+impl<Value, Channel> RadioAntenna<Value, Channel>
 where
     Channel: RadioChannel,
 {
     pub fn new(
-        station: RadioStation<Sound, Channel>,
+        station: RadioStation<Value, Channel>,
         scope_id: ScopeId,
-    ) -> Radioantenna<Sound, Channel> {
-        Radioantenna { station, scope_id }
+    ) -> RadioAntenna<Value, Channel> {
+        RadioAntenna { station, scope_id }
     }
 }
 
-impl<Sound, Channel> Drop for Radioantenna<Sound, Channel>
+impl<Value, Channel> Drop for RadioAntenna<Value, Channel>
 where
     Channel: RadioChannel,
 {
@@ -94,56 +94,56 @@ where
     }
 }
 
-pub struct RadioGuard<'a, Sound, Channel>
+pub struct RadioGuard<'a, Value, Channel>
 where
     Channel: RadioChannel,
 {
-    antenna: &'a Rc<Radioantenna<Sound, Channel>>,
+    antenna: &'a Rc<RadioAntenna<Value, Channel>>,
     channel: Channel,
-    value: RefMut<'a, Sound>,
+    value: RefMut<'a, Value>,
 }
 
-impl<'a, Sound, Channel> Drop for RadioGuard<'a, Sound, Channel>
+impl<'a, Value, Channel> Drop for RadioGuard<'a, Value, Channel>
 where
     Channel: RadioChannel,
 {
     fn drop(&mut self) {
-        self.antenna.station.play(&self.channel)
+        self.antenna.station.notify_listeners(&self.channel)
     }
 }
 
-impl<'a, Sound, Channel> Deref for RadioGuard<'a, Sound, Channel>
+impl<'a, Value, Channel> Deref for RadioGuard<'a, Value, Channel>
 where
     Channel: RadioChannel,
 {
-    type Target = RefMut<'a, Sound>;
+    type Target = RefMut<'a, Value>;
 
-    fn deref(&self) -> &RefMut<'a, Sound> {
+    fn deref(&self) -> &RefMut<'a, Value> {
         &self.value
     }
 }
 
-impl<'a, Sound, Channel> DerefMut for RadioGuard<'a, Sound, Channel>
+impl<'a, Value, Channel> DerefMut for RadioGuard<'a, Value, Channel>
 where
     Channel: RadioChannel,
 {
-    fn deref_mut(&mut self) -> &mut RefMut<'a, Sound> {
+    fn deref_mut(&mut self) -> &mut RefMut<'a, Value> {
         &mut self.value
     }
 }
 
-pub struct Radio<Sound, Channel>
+pub struct Radio<Value, Channel>
 where
     Channel: RadioChannel,
 {
-    antenna: Rc<Radioantenna<Sound, Channel>>,
+    antenna: Rc<RadioAntenna<Value, Channel>>,
 }
 
-impl<Sound, Channel> Radio<Sound, Channel>
+impl<Value, Channel> Radio<Value, Channel>
 where
     Channel: RadioChannel,
 {
-    pub fn new(antenna: Rc<Radioantenna<Sound, Channel>>) -> Radio<Sound, Channel> {
+    pub fn new(antenna: Rc<RadioAntenna<Value, Channel>>) -> Radio<Value, Channel> {
         Radio { antenna }
     }
 
@@ -153,8 +153,8 @@ where
     /// ```rs
     /// let value = radio.read();
     /// ```
-    pub fn read(&self) -> Ref<Sound> {
-        self.antenna.station.sound.borrow()
+    pub fn read(&self) -> Ref<Value> {
+        self.antenna.station.value.borrow()
     }
 
     /// Modify the state using the channel this radio was created with.
@@ -164,14 +164,14 @@ where
     /// ```rs
     /// radio.write().value = 1;
     /// ```
-    pub fn write(&self) -> RadioGuard<Sound, Channel> {
+    pub fn write(&self) -> RadioGuard<Value, Channel> {
         RadioGuard {
             channel: self
                 .antenna
                 .station
                 .get_scope_channel(self.antenna.scope_id),
             antenna: &self.antenna,
-            value: self.antenna.station.sound.borrow_mut(),
+            value: self.antenna.station.value.borrow_mut(),
         }
     }
 
@@ -181,26 +181,26 @@ where
     /// ```rs, no_run
     /// radio.write(Channel::Whatever).value = 1;
     /// ```
-    pub fn write_with(&self, channel: Channel) -> RadioGuard<Sound, Channel> {
+    pub fn write_with(&self, channel: Channel) -> RadioGuard<Value, Channel> {
         RadioGuard {
             channel,
             antenna: &self.antenna,
-            value: self.antenna.station.sound.borrow_mut(),
+            value: self.antenna.station.value.borrow_mut(),
         }
     }
 }
 
-pub fn use_radio<Sound: 'static, Channel: 'static>(
+pub fn use_radio<Value: 'static, Channel: 'static>(
     cx: &ScopeState,
     channel: Channel,
-) -> &Radio<Sound, Channel>
+) -> &Radio<Value, Channel>
 where
     Channel: RadioChannel,
 {
-    let station = use_context::<RadioStation<Sound, Channel>>(cx).unwrap();
+    let station = use_context::<RadioStation<Value, Channel>>(cx).unwrap();
 
     let radio = cx.use_hook(|| {
-        let antenna = Radioantenna::new(station.clone(), cx.scope_id());
+        let antenna = RadioAntenna::new(station.clone(), cx.scope_id());
         Radio::new(Rc::new(antenna))
     });
 
@@ -209,16 +209,16 @@ where
     radio
 }
 
-pub fn use_init_radio_station<Sound: 'static, Channel: 'static>(
+pub fn use_init_radio_station<Value: 'static, Channel: 'static>(
     cx: &ScopeState,
-    init_value: impl FnOnce() -> Sound,
-) -> &RadioStation<Sound, Channel>
+    init_value: impl FnOnce() -> Value,
+) -> &RadioStation<Value, Channel>
 where
     Channel: RadioChannel,
 {
     use_context_provider(cx, || RadioStation {
-        sound: Rc::new(RefCell::new(init_value())),
-        play_button: cx.schedule_update_any(),
+        value: Rc::new(RefCell::new(init_value())),
+        schedule_update_any: cx.schedule_update_any(),
         listeners: Rc::default(),
     })
 }
